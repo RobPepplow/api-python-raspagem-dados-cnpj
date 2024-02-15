ae3c76a55debe811806484a266c24ccceeb9bd60
ROBSON PEPPLOW LOPES <robsonpepplow@gmail.com>
2024-02-15 18:36:43 -0300

    altação dos dados e payload e tbm para pegar a incrição municipal se é de CTBA

--- getInscricao.py
+++ getInscricao.py
@@ -1,8 +1,8 @@
 import requests
 
 def getInscricao(cnpj: str) -> str:
-     # url = 'https://bot-busca-im-ctba-l3cizs4bqa-uc.a.run.app/api/crawler/insc-municipal/pr/ctba'
-    url = 'http://127.0.0.1:5000/api/crawler/insc-municipal/pr/ctba'
+    url = 'https://bot-busca-im-ctba-l3cizs4bqa-uc.a.run.app/api/crawler/insc-municipal/pr/ctba'
+    #url = 'http://127.0.0.1:5000/api/crawler/insc-municipal/pr/ctba'
     dados = {'cnpj': cnpj}
     resposta = requests.post(url, json=dados)
     
